40b112c80d426f2a6d882dbfcd52df042acded4c
thiago-figueiredo <tfigueiredo@gmail.com>
2021-05-24 18:22:36 -0300

    And removing an unnecessary intermediary component.

--- src/home/home.vue
+++ src/home/home.vue
@@ -1,15 +1,42 @@
 <template>
-  <hello-world msg="Messages by Twiage" />
+  <div class="mt-5 splash">
+    <h1>Messages by Twiage</h1>
+    <form>
+      <textarea @input="login({ token: $event.target.value })" placeholder="Paste your token here"></textarea>
+    </form>
+    <a
+      href="https://www.deviantart.com/dio-03/art/MEKANO-Retrofix-d-201919932"
+      target="_blank"
+    >
+      <img
+        alt="Mekano"
+        class="image mb-2"
+        src="/src/home/assets/mekano-big-screenRS.jpg"
+      />
+    </a>
+  </div>
 </template>
 
+<style scoped>
+.splash {
+  text-align: center;
+}
+.image {
+  max-width: 100vw;
+  width: 600px;
+}
+</style>
+
 <script>
-import { defineComponent } from "vue";
+export default {
+  setup() {
+    const login = ({ token }) => {
+      localStorage.setItem("token", token);
 
-import helloWorld from "./components/hello-world.vue";
+      window.location.reload();
+    };
 
-export default defineComponent({
-  components: {
-    helloWorld,
+    return { login };
   },
-});
+};
 </script>
